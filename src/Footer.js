5025ff5ba4c038a25f6e6280fb7ad842be70c0a7
Pippa101 <94748151+Pippa101@users.noreply.github.com>
2023-04-24 16:13:54 +0100

    adding linkedIn Link

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,11 @@ const Footer = () => {
     <footer>
       <h3>Contact me</h3>
       <ul id="contact-info">
-        <li>pipsworkemail@gmail.com</li>
+        <li>
+          <a href="www.linkedin.com/in/pippa-smith-a9b128217">
+            pipsworkemail@gmail.com
+          </a>
+        </li>
         <li>LinkedIn</li>
       </ul>
     </footer>
