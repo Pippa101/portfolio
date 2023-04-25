a9c14bde46e7f3514d8785e57f8683965eda9df6
Pippa101 <94748151+Pippa101@users.noreply.github.com>
2023-04-25 16:59:18 +0100

    using media queries to create responsive design

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ function App() {
   return (
     <div className="App">
       <img src={require("./images/Banner.jpg")} id="hero-img" alt="my art" />
+      <nav id="navbar">
+        <ul>
+          <li>Artwork and Style</li>
+          <li>Software Development</li>
+          <li>About</li>
+        </ul>
+      </nav>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/Artwork" element={<Artwork />} />
--- src/Homepage.js
+++ src/Homepage.js
@@ -13,6 +13,13 @@ const Homepage = () => {
           <h2>Artwork and Style</h2>
         </section>
       </Link>
+      <article id="art-extra-info" className="extra-info">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Ut enim ad minim veniam, quis nostrud exercitation
+        ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      </article>
       <section id="software-development" className="tile">
         <img
           src={require("./images/hair-serum-bottle copy.jpeg")}
@@ -21,13 +28,28 @@ const Homepage = () => {
         />
         <h2>Software Development</h2>
       </section>
+      <article id="dev-extra-info" className="extra-info">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Ut enim ad minim veniam, quis nostrud exercitation
+        ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      </article>
       <section id="about" className="tile">
         <img
           src={require("./images/smart-profile-1.jpg")}
           className="nav-img"
           alt="portrait of Author"
         />
-        <h2>About Me</h2>
+        <article>
+          <h2>About</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </article>
       </section>
     </div>
   );
